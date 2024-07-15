d9215fe350fdc25e4bb3f278174d57d81b20eb9d
arensin8 <arensinani4@gmail.com>
2024-07-15 19:19:15 +0400

    some commands

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
+const path = require("path");
+const { createReadStream } = require("fs");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const helpMessage = `
@@ -8,6 +10,7 @@ const helpMessage = `
  /help - Find your answers
  /print - prints sth
  /print with parameter - prints your text
+ /cities - Shows the list of the greatest cities of the world
 `;
 
 bot.start((ctx) => {
@@ -29,6 +32,33 @@ bot.command("print", (ctx) => {
     message = msgList.slice(1).join(" ");
   }
   ctx.reply(message);
+});
+
+bot.command("cities", (ctx) => {
+  const citiesList = `
+ /Yerevan - Armenia
+ /Paris - France
+ /madrid - Spain
+`;
+  ctx.reply(citiesList);
+});
+
+bot.command(["yerevan", "Yerevan"], (ctx) => {
+  ctx.sendPhoto({
+    source: createReadStream(path.join(__dirname, "photos", "yerevan.jpg")),
+  });
+});
+
+bot.command(["Paris", "paris"], (ctx) => {
+  ctx.sendPhoto({
+    source: createReadStream(path.join(__dirname, "photos", "paris.jpg")),
+  });
+});
+
+bot.command(["Madrid", "madrid"], (ctx) => {
+  ctx.sendPhoto({
+    source: createReadStream(path.join(__dirname, "photos", "madrid.jpg")),
+  });
 });
 
 bot.launch();
